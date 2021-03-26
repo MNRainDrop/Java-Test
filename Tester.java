3ccf07702447a915b19bc84528dadbe78e6a3f71
MNRainDrop <marloneninjaV2@gmail.com>
2021-03-26 11:54:03 -0700

    Added pyramid print
    Used nested for loop to make half a pyramid

--- Tester.java
+++ Tester.java
@@ -1,6 +1,21 @@
-
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        for (int i = 0; i < 5; i++){
+            String s = "";
+            for (int j = 5; j > i; j--){
+                s += "* ";
+            }
+            System.out.println(s);
+        }
+
+        for (int i = 0; i < 5; i++){
+            String s = "";
+            for (int j = 0; j <= i; j++){
+                s += "* ";
+            }
+            System.out.println(s);
+        }
     }
 }
